0174af427bef901ea7936ecae715c6ff0a224484
naritotakizawa <toritoritorina@gmail.com>
2021-02-05 14:10:04 +0900

    first commit

---
+++ frontend/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2 v-for="user in users" :key="user.id">{{ user.username }}</h2>
+    <button @click="$fetch">リロード</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async fetch() {
+    this.users = await this.$axios.$get('/auth/users/')
+  },
+}
+</script>
+
+<style></style>
